29d3025f203b622bfe680429221010c47a642b1f
ErikaNunex <erikanunes0512@gmail.com>
2022-09-15 10:25:34 -0300

    contando aluno pela a quantidade de elementos do array

---
+++ eventos.js
@@ -0,0 +1,13 @@
+let turma =[
+    'Alexandre',
+    'Erika',
+    'Davi',
+    'Igor',
+]
+
+turma.push('Alicia');
+turma.push('Italo');
+
+for(let n =0; n<6; n++){
+    document.write(turma[n]+ '<br>');
+}
